06183a8689db710cd84ea33ff84d823777486842
joshnos <joshuanostas49@gmail.com>
2024-08-16 20:21:25 +0300

    Update food page widget

---
+++ lib/presentation/pages/food_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/food_bloc/food_bloc.dart';
+
+class FoodPage extends StatelessWidget {
+  final int foodId;
+
+  const FoodPage({super.key, required this.foodId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Food Details'),
+      ),
+      body: BlocConsumer<FoodBloc, FoodState>(
+        listener: (context, state) {
+          if (state is FoodError) {
+            // Handle error
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Error:')),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is FoodLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is FoodLoaded) {
+            final food = state.food;
+            return Column(
+              children: [
+                Text(food.description),
+              ],
+            );
+          } else {
+            return const Center(child: Text('No data available.'));
+          }
+        },
+      ),
+    );
+  }
+}
