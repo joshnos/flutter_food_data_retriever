f9dea0d079f284a4d3a59438a83f3af4539f06b4
joshnos <joshuanostas49@gmail.com>
2024-08-16 20:21:17 +0300

    Update the food repository implementation

--- lib/data/repositories/food_repository_impl.dart
+++ lib/data/repositories/food_repository_impl.dart
@@ -1,32 +1,29 @@
 import 'package:dartz/dartz.dart';
 
-import '../../core/errors/exceptions.dart';
+import '../../core/errors/failures.dart';
 import '../../core/errors/failures.dart';
 import '../../core/network/network_info.dart';
-import '../entities/food.dart';
-import '../repositories/food_repository.dart';
-import '../../core/util/input_converter.dart';
+import '../../domain/entities/food.dart';
+import '../../domain/repositories/food_repository.dart';
 import '../../data/datasources/food_remote_data_source.dart';
 import '../../data/models/food_model.dart';
 
 class FoodRepositoryImpl implements FoodRepository {
   final FoodRemoteDataSource remoteDataSource;
   final NetworkInfo networkInfo;
-  final InputConverter inputConverter;
 
   FoodRepositoryImpl({
     required this.remoteDataSource,
     required this.networkInfo,
-    required this.inputConverter,
   });
 
   @override
   Future<Either<Failure, Food>> getFoodDetails(int foodId) async {
     if (await networkInfo.isConnected) {
       try {
-        final remoteFood = await remoteDataSource.getFood(foodId);
+        final remoteFood = await remoteDataSource.getFoodDetails(foodId);
         return Right(remoteFood.toDomain());
-      } on ServerException {
+      } on ServerFailure {
         return Left(ServerFailure());
       }
     } else {
